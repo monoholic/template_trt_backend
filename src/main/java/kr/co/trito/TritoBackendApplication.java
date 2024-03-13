d5a20f6da02b1ab5d6ac8135be4f8ec995fa5d57
elrdan <seongo9655@gmail.com>
2024-03-13 14:28:16 +0900

    git init

---
+++ src/main/java/kr/co/trito/TritoBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co.trito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TritoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TritoBackendApplication.class, args);
+    }
+
+}
