8011b5216b3f76bf4e3cc16f2045b2f619fa1fc2
elrdan <seongo9655@gmail.com>
2024-03-15 15:19:02 +0900

    fix: flyway를 적용하면서 column 명의 불일치로 인해 @Column을 사용해서 수정

--- src/main/java/kr/co/trito/domain/UserInfo.java
+++ src/main/java/kr/co/trito/domain/UserInfo.java
@@ -2,27 +2,35 @@ package kr.co.trito.domain;
 
 import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
+import java.sql.Date;
 
 @Entity
-@Table(name = "USER_INFO")
+@Table( name = "USER_INFO" )
 public class UserInfo {
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    private String user_id;
+    @Id
+    @Column(name = "user_id")
+    private String userId;
 
     private String passwd;
 
-    private String user_nm;
+    @Column(name = "user_nm")
+    private String userNm;
 
-    private String sawon_no;
+    @Column(name = "sawon_no")
+    private String sawonNo;
 
-    private String use_yn;
+    @Column(name = "use_yn")
+    private String useYn;
 
-    private LocalDateTime reg_date;
+    @Column(name = "reg_date")
+    private Date regDate;
 
-    private LocalDateTime upt_date;
+    @Column(name = "upt_date")
+    private Date uptDate;
 
-    private String reg_id;
+    @Column(name = "reg_id")
+    private String regId;
 
-    private String upt_id;
+    @Column(name = "upt_id")
+    private String uptId;
 }
