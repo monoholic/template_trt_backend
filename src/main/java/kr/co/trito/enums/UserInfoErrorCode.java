88fdc65552e284217338238a8fbd85e1cc10a7ed
elrdan <seongo9655@gmail.com>
2024-03-19 15:42:40 +0900

    fix: UserInfo 에러 메시지 수정

--- src/main/java/kr/co/trito/enums/UserInfoErrorCode.java
+++ src/main/java/kr/co/trito/enums/UserInfoErrorCode.java
@@ -6,8 +6,9 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum UserInfoErrorCode implements BaseErrorCode {
-    NOT_FIND_MEMBER_EMAIL(400, "존재하지 않는 회원입니다.", HttpStatus.BAD_REQUEST),
-    DUPLICATED_MEMBER_EMAIL(400, "이미 존재하는 회원 이메일입니다.", HttpStatus.BAD_REQUEST);
+
+    NOT_MATCH_USER_INFO(400, "아이디 또는 비밀번호를 확인해주세요.", HttpStatus.BAD_REQUEST),
+    NOT_FIND_USER_INFO(400, "존재하지 않는 회원입니다.", HttpStatus.BAD_REQUEST);
 
     private final int errorCode;
     private final String errorMessage;
