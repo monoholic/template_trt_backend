ede3cc734a4101a5d546a0713a5bb6aaf8d6a893
elrdan <seongo9655@gmail.com>
2024-03-25 10:36:52 +0900

    feat: jpaQueryFactory 설정 추가

--- src/main/java/kr/co/trito/TritoBackendApplication.java
+++ src/main/java/kr/co/trito/TritoBackendApplication.java
@@ -1,13 +1,21 @@
 package kr.co.trito;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TritoBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TritoBackendApplication.class, args);
+    }
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 
 }
