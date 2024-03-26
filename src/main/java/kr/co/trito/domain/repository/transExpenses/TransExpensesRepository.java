4bfdf462c2cb910de15ca5bf63d46fa8dbc99244
elrdan <seongo9655@gmail.com>
2024-03-26 13:19:29 +0900

    style: 주석제거

--- src/main/java/kr/co/trito/domain/repository/transExpenses/TransExpensesRepository.java
+++ src/main/java/kr/co/trito/domain/repository/transExpenses/TransExpensesRepository.java
@@ -2,8 +2,6 @@ package kr.co.trito.domain.repository.transExpenses;
 
 import kr.co.trito.domain.Allowance;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface TransExpensesRepository extends JpaRepository<Allowance, String>, TransExpensesCustom {
 }
