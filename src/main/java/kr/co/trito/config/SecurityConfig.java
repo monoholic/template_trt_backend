9162f8acb7ff7923a7dc0120c6044fbb7a06f78c
kjw <kjo6391@gmail.com>
2024-09-05 10:49:46 +0900

    김정욱 - 사용자관리 내용, 파일 추가 // 마이파티스 로그 추가 // 시큐리티 권한 추가

--- src/main/java/kr/co/trito/config/SecurityConfig.java
+++ src/main/java/kr/co/trito/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
         http.httpBasic((auth) -> auth.disable());
 
         http.authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/login", "/", "/join", "/userMng/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/reissue").permitAll()
                         .anyRequest().authenticated());
